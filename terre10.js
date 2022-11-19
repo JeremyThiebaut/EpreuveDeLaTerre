10ea3606782ec160662693fd20c9dd1b8436f7ff
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-11-19 20:01:48 +0100

    Primary number done

---
+++ terre10.js
@@ -0,0 +1,20 @@
+const primary = () => {
+  const nbr = process.argv;
+  nbr.splice(0, 2);
+  if (nbr.length > 1) {
+    return "error";
+  }
+
+  if (nbr[0] < 2) {
+    return `Non, ${nbr[0]} n,est pas un nombre premier.`;
+  }
+
+  for (let i = 2; i <= Math.sqrt(nbr[0]); i++) {
+    if (nbr[0] % i == 0) {
+      return `Non, ${nbr[0]} n,est pas un nombre premier.`;
+    }
+    return `Oui, ${nbr[0]} est un nombre premier.`;
+  }
+};
+
+console.log(primary());
