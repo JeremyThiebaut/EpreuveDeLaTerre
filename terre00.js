d9835622fff16ca95d03765522346c3359247dd7
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-11-19 07:53:08 +0100

    ended

---
+++ terre00.js
@@ -0,0 +1,41 @@
+const AZ = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const alphabet = () => {
+  let text = "";
+
+  AZ.forEach((letter) => {
+    text = text + letter;
+  });
+
+  text = text + "\n";
+  return text;
+};
+
+console.log(alphabet());
