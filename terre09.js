07dde86445253debc41a0701be7c6bbd04d9eab1
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-11-19 19:38:20 +0100

    square root of a number

---
+++ terre09.js
@@ -0,0 +1,11 @@
+const rasCar = () => {
+  const nbr = process.argv;
+  nbr.splice(0, 2);
+  if (nbr.length > 1 || nbr[0] < 0) {
+    return "error";
+  }
+
+  return Math.sqrt(nbr[0]);
+};
+
+console.log(rasCar());
