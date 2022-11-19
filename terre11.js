5b3d8833ce1747f2f1460011bd0ddf51ff06318d
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2022-11-19 21:22:41 +0100

    formate time done

---
+++ terre11.js
@@ -0,0 +1,39 @@
+const clock = () => {
+  const clc = process.argv;
+
+  if (process.argv.length < 3) {
+    return "choose a time (ex: node exo.js 13:47)";
+  }
+
+  clc.splice(0, 2);
+
+  if (clc.length > 1) {
+    return "choose a time (ex: node exo.js 13:47)";
+  }
+
+  const date = clc[0].split(":");
+
+  const hour = date[0];
+  const suffix = hour >= 12 ? "pm" : "am";
+  const minute = date[1];
+  let hours = hour % 12 || 12;
+
+  if (hour == 12 || hour == 00) {
+    hours = 00;
+  }
+
+  if (
+    hour > 23 ||
+    hour < 0 ||
+    hour.length < 2 ||
+    minute < 0 ||
+    minute > 59 ||
+    minute.length < 2
+  ) {
+    return "the time entered is invalid (ex: node exo.js 13:47)";
+  }
+
+  return `${hours}:${minute}${suffix}`;
+};
+
+console.log(clock());
